44b21a7af53924071418e96b4c1d39a39bb914ad
leopapadopoulos6 <90059812+leopapadopoulos6@users.noreply.github.com>
2021-10-07 17:01:21 -0400

    Add files via upload

---
+++ HW4a.py
@@ -0,0 +1,38 @@
+import requests
+import json
+import unittest
+#from github import Github
+#import pprint
+def github():
+    token = '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'
+    ID = input("What is your Github username: ")
+    url = f"https://api.github.com/users/{ID}/repos"
+
+
+    response_repo = requests.get(url)
+    data_repo = response_repo.json()
+
+    for repository in data_repo:
+        repo = repository["name"]
+        print("Repo: ", repo)
+
+        url_commit = f"https://api.github.com/repos/{ID}/{repo}/commits"
+        response_commit = requests.get(url_commit)
+        data_commit = response_commit.json()
+        
+        commit_count = len(data_commit)
+        #for commit in data_commit:
+        #    c = commit["sha"]
+        #    n = 0     
+        #    if len(c) == 40:
+        #        n =+ 1        
+        print("Commit info: ", commit_count)
+        
+#        for commit["sha"] in c:
+#               n = len(c)
+github()
+
+class testingCommit(unittest.TestCase):
+    def testInput(self):
+        self.assertEqual(github())
+
