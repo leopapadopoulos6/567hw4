aa0abdcd3a97943f121759c821402512c6bc64d6
leopapadopoulos6 <90059812+leopapadopoulos6@users.noreply.github.com>
2021-10-09 00:02:11 -0400

    Add files via upload

---
+++ test_hw4a.py
@@ -0,0 +1,16 @@
+import unittest
+from HW4a import github
+
+
+class TestHW4a(unittest.TestCase):
+    def testCase1(self):
+        self.assertEqual(github('leopapadopoulos6'), ['Repo: 567hw4 Commit info: 11',
+                                                 'Repo: hw01testingtriangles Commit info: 1',
+                                                 'Repo: p3-lpapadopoulos Commit info: 1',
+                                                 'Repo: ssw567 Commit info: 2',
+                                                 'Repo: Triangle567 Commit info: 11'])
+
+
+if __name__ == '__main__':
+    print('Running unit tests')
+    unittest.main()
