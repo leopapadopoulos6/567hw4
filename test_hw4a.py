0fe65c637a7c194fc36785afc2f461ec48f8baa1
leopapadopoulos6 <90059812+leopapadopoulos6@users.noreply.github.com>
2021-10-09 19:50:09 -0400

    Update test_hw4a.py

--- test_hw4a.py
+++ test_hw4a.py
@@ -4,7 +4,7 @@ from HW4a import github
 
 class TestHW4a(unittest.TestCase):
     def testCase1(self):
-        self.assertEqual(github('leopapadopoulos6'), ['Repo: 567hw4 Commit info: 47',
+        self.assertEqual(github('leopapadopoulos6'), ['Repo: 567hw4 Commit info: 30',
                                                  'Repo: hw01testingtriangles Commit info: 1',
                                                  'Repo: hw01testingtriangles Commit info: 1',
                                                  'Repo: p3-lpapadopoulos Commit info: 1',
